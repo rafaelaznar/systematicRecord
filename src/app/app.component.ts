a4a7e1fb386f51496fdc4962f25494dd8a72df85
RAFAEL AZNAR <rafaaznar@gmail.com>
2023-01-20 19:14:09 +0100

    posts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'andamioClient';
+  title = 'systematic record';
   constructor() {
     console.log("app.component: token", localStorage.getItem("token"));
   }
